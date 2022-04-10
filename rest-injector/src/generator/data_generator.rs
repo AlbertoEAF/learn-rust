06d4f822d236d13fb143ea5aeed7c1251abe9483
Alberto Ferreira <AlbertoEAF@users.noreply.github.com>
2022-04-10 13:35:38 +0100

    Working asynchronous

--- rest-injector/src/generator/data_generator.rs
+++ rest-injector/src/generator/data_generator.rs
@@ -3,10 +3,11 @@ extern crate threads_pool;
 use std::collections::HashMap;
 use serde_json::Value;
 use std::sync::atomic;
+use std::sync::Arc;
 
 pub struct DatasetGenerator {
     num_features: usize,
-    counter: atomic::AtomicI64,
+    pub counter: atomic::AtomicI64,
     feature_names: Vec<String>,
 }
 
@@ -27,20 +28,23 @@ impl DatasetGenerator {
         }
     }
 
+    pub fn next(&mut self) -> i64 {
+        self.counter.fetch_add(1, atomic::Ordering::SeqCst)
+    }
 
-    pub fn next(&mut self) -> String {
-        let value = self.counter.fetch_add(1, atomic::Ordering::SeqCst);
+
+    pub fn gen(&self, value: i64) -> String {
 
         let mut data = Datapoint::with_capacity(self.num_features.try_into().unwrap());
         for f in 0..self.num_features {
             //let name = format!("f_{}", f);                        
-            let name = self.feature_names.get(f).unwrap().clone();
-            data.insert(name, Value::from(value));            
+            let name = self.feature_names.get(f).unwrap();
+            data.insert(name.to_string(), Value::from(value));            
         }
         serde_json::json!(data).to_string()
     }
 }
 
 
-unsafe impl Send for DatasetGenerator {}
-unsafe impl Sync for DatasetGenerator {}
+//unsafe impl Send for DatasetGenerator {}
+//unsafe impl Sync for DatasetGenerator {}
--- rest-injector/src/main.rs
+++ rest-injector/src/main.rs
@@ -5,26 +5,31 @@ use threads_pool::*;
 mod generator;
 
 use std::time::Instant;
-use std::sync::mpsc::channel;
+use std::sync::mpsc;
 use std::thread;
+use std::sync::Arc;
 
 fn main() {
 
     let pool = ThreadPool::new(4);
 
-    let mut generator = generator::data_generator::DatasetGenerator::new(3000);
+    let mut generator = Arc::new(generator::data_generator::DatasetGenerator::new(3000));
 
-    let N = 1000;
-    let (tx, rx) = channel();
+    const N: i64 = 10000;
+    let (tx, rx) = mpsc::channel();
 
-    for _ in 0..N {
+    for i in 0..N {
         
-        unsafe {
+        let tx = tx.clone();
+
+        generator.clone().counter.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+        let gen = generator.clone();
+        
         //let p = generator.next();
-            pool.execute(move || {
-                tx.clone().send(generator.next()).unwrap();           
-            });
-        }
+        pool.execute(move || {
+            tx.send(gen.gen(i)).unwrap();                     
+        }).unwrap();
+        
     }
 
     for _ in 0..N {
