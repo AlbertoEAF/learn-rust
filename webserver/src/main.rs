5b3099a8f38af4440386b686dbc3f47626c7e5de
Alberto Ferreira <AlbertoEAF@users.noreply.github.com>
2022-04-10 01:30:52 +0100

    Finish async server

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,28 +1,54 @@
 // https://doc.rust-lang.org/book/ch20-01-single-threaded.html
 
+extern crate threads_pool;
+use threads_pool::*;
+
+//use webserver::ThreadPool;
+use std::thread;
+use std::time::Duration;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(8);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+        
 
-        handle_connection(stream);
+        pool.execute(|| {
+            println!("!submitting!");
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down!");
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
 
     let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
         contents.len(),
         contents
     );
