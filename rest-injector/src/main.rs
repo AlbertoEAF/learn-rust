cfaed4267f583e5fa81c9b14f191f2094518d5bd
Alberto Ferreira <alberto.ferreira@feedzai.com>
2022-04-11 10:37:04 +0100

    Generator doesn't execute all

--- rest-injector/src/main.rs
+++ rest-injector/src/main.rs
@@ -13,31 +13,33 @@ fn main() {
     const N: i64 = 12;  // Increase this if you're not getting the deadlock yet, or run cargo run again until it happens.
     let (tx, rx) = mpsc::channel();
 
-    let tx_producer = tx.clone();
     let producer_thread = thread::spawn(move || {
-        let pool = ThreadPool::new(4);
+        let mut pool = ThreadPool::new(4);
+
         let generator = Arc::new(generator::data_generator::DatasetGenerator::new(3000));
         for i in 0..N {
-            println!("Generating #{}", i);
-            let tx_ref = tx_producer.clone();
+            println!("> Generate #{}", i);       
+            let tx_ref = tx.clone();
             let generator_ref = generator.clone();
             pool.execute(move || {
+                println!("> Generate #{}: Thread executed!", i);
                 tx_ref.send(generator_ref.next()).expect("tx failed.");              // This fails!
                 //tx_ref.send(format!(" {}            ", i)).expect("tx failed.");   // This works!
             })
             .unwrap();
-        }
-
+        }        
+        pool.close();
         println!("Generator done!");
     });
 
-    println!("-» Consumer consuming!");
+    println!("  -» Consumer consuming!");
     for j in 0..N {
         let s = rx.recv().expect("rx failed");
-        println!("-» Consumed #{}:   {} ...     ", j, &s[..10]);
+        println!("  -» Consumed #{}:   {} ...     ", j, &s[..10]);
     }
-    println!("Consumer done!!");
+    println!("  -» Consumer done!!");
 
     producer_thread.join().unwrap();
+    
     println!("Success. Exit!");
 }
