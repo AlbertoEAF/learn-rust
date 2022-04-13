584ce382c897182ffe103fa7ddcb0675f377bb0e
Alberto Ferreira <alberto.ferreira@feedzai.com>
2022-04-13 23:21:03 +0100

    Compilation error

--- rest-injector/src/generator/data_generator.rs
+++ rest-injector/src/generator/data_generator.rs
@@ -11,44 +11,6 @@ pub struct DatasetGenerator {
 
 type Out = String;
 
-/*
-struct EventJSON {
-    serializer: Option<serde_json::ser::Serializer<Vec<u8>>>,
-    map: Option<
-        serde_json::ser::Compound<'static, std::vec::Vec<u8>, serde_json::ser::CompactFormatter>,
-    >,
-}
-
-impl EventJSON {
-    pub fn new(size: usize) -> EventJSON {
-
-        let mut serializer = serde_json::ser::Serializer::new(Vec::new());
-        let map = Some(serializer.serialize_map(Some(size)).unwrap());
-
-        EventJSON {
-            serializer: None,
-            map: map,
-        }
-    }
-
-    pub fn insert(&mut self, k: &str, v: &str) {
-        if let Some(map) = &mut self.map {
-            map.serialize_entry(k, v);
-        } else {
-            panic!("Already serialized! Unusable object!");
-        }
-    }
-
-    pub fn to_string(&mut self) -> String {
-        if let Some(serializer) = self.serializer.take() {
-            String::from_utf8(serializer.into_inner()).expect("Invalid UTF-8.")
-        } else {
-            panic!("Already serialized! Unusable object!");
-        }
-    }
-}
-*/
-
 impl DatasetGenerator {
     pub fn new(num_features: usize) -> DatasetGenerator {
         let mut feature_names = Vec::new();
@@ -72,21 +34,20 @@ impl DatasetGenerator {
 
     /// Generates the ith item in the sequence.
     pub fn gen(&self, ith: i64) -> Out {
-
+        //let mut ev = EventJSON::new(self.num_features);
         let mut serializer = serde_json::ser::Serializer::new(Vec::new());
         let mut map = serializer.serialize_map(Some(self.num_features)).unwrap();
-        //let mut ev = EventJSON::new(self.num_features);
 
         for f in 0..self.num_features {
             let name = self.feature_names.get(f).unwrap();
 
+            //ev.insert(name, &ith.to_string());
             map.serialize_entry(name, &ith.to_string())
                 .expect("Can't serialize feature {}.");
-            //ev.insert(name, &ith.to_string());
         }
         map.end().expect("Serialization failed.");
 
+        //ev.serialize()
         String::from_utf8(serializer.into_inner()).expect("Invalid UTF-8.")
-        //ev.to_string()
     }
 }
--- rest-injector/src/lib.rs
+++ rest-injector/src/lib.rs
@@ -1,4 +1,40 @@
 //! Lib!
-//! 
+//!
 
 pub mod generator;
+
+use serde::ser::{SerializeMap, Serializer};
+//use std::vec::Vec;
+
+struct EventJSON {
+    serializer: Option<serde_json::ser::Serializer<Vec<u8>>>,
+    map: Option<serde_json::ser::Compound<'static, Vec<u8>, serde_json::ser::CompactFormatter>>,
+}
+
+impl EventJSON {
+    pub fn new(size: usize) -> EventJSON {
+        let mut serializer = serde_json::ser::Serializer::new(Vec::new());
+        let mut map = serializer.serialize_map(Some(size)).unwrap();
+
+        EventJSON {
+            serializer: Some(serializer),
+            map: Some(map),
+        }
+    }
+
+    pub fn insert(&mut self, k: &str, v: &str) {
+        if let Some(map) = &mut self.map {
+            map.serialize_entry(k, v);
+        } else {
+            panic!("Already serialized! Unusable object!");
+        }
+    }
+
+    pub fn serialize(&mut self) -> String {
+        if let Some(serializer) = self.serializer.take() {
+            String::from_utf8(serializer.into_inner()).expect("Invalid UTF-8.")
+        } else {
+            panic!("Already serialized! Unusable object!");
+        }
+    }
+}
