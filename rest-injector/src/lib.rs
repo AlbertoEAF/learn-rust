c4dfe0ebd64de7fea100cc55ccf4bba1ac845f7e
Alberto Ferreira <alberto.ferreira@feedzai.com>
2022-11-14 14:37:12 +0000

    Push changes I found lying around

--- rest-injector/src/lib.rs
+++ rest-injector/src/lib.rs
@@ -6,20 +6,26 @@ pub mod generator;
 use serde::ser::{SerializeMap, Serializer};
 //use std::vec::Vec;
 
-struct EventJSON {
-    serializer: Option<serde_json::ser::Serializer<Vec<u8>>>,
-    map: Option<serde_json::ser::Compound<'static, Vec<u8>, serde_json::ser::CompactFormatter>>,
+extern crate owning_ref;
+use owning_ref::BoxRef;
+
+struct EventJSON<'a> {
+    serializer: Option<Box<serde_json::ser::Serializer<Vec<u8>>>>,
+    map: Option<Box<&'a serde_json::ser::Compound<'a, Vec<u8>, serde_json::ser::CompactFormatter>>>,
 }
 
-impl EventJSON {
-    pub fn new(size: usize) -> EventJSON {
-        let mut serializer = serde_json::ser::Serializer::new(Vec::new());
-        let mut map = serializer.serialize_map(Some(size)).unwrap();
+impl<'a> EventJSON<'a> {
+    pub fn new(size: usize) -> (std::boxed::Box<serde_json::Serializer<std::vec::Vec<u8>>>, std::boxed::Box<serde_json::ser::Compound<'a, std::vec::Vec<u8>, serde_json::ser::CompactFormatter>>) {
 
-        EventJSON {
+        let mut serializer = Box::new(serde_json::ser::Serializer::new(Vec::new()));
+
+        let mut map = Box::new(serializer.serialize_map(Some(size)).unwrap());
+
+        (serializer, map)
+        /*EventJSON {
             serializer: Some(serializer),
             map: Some(map),
-        }
+        }*/
     }
 
     pub fn insert(&mut self, k: &str, v: &str) {
